a72f4a50e9f7f6d5e77a3254d0a6f1e4d0ac3195
Zeyu-Li <zeyu.li23@gmail.com>
2021-06-01 18:08:49 -0600

    added test file

--- test/test.py
+++ test/test.py
@@ -1,3 +1,17 @@
-import * from '../main.py'
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
 
+from main import Transaction, Block, Chain, Wallet
+from bottom import to_bottom, from_bottom
+
+test_tran = Transaction(1500, to_bottom("genesis"), to_bottom("pinguin0"))
+
+print(test_tran.get_string())
+
+test_block = Block(None, test_tran)
+
+print(test_block.get_hash())
+# print(to_bottom("genesis"))
 
